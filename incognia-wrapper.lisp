8bddd79aa28344604b7f21da3ca0f3a10de675fc
Alan Gomes <alan@inloco.com.br>
2021-01-14 23:18:22 -0300

    Implements onboarding-signups API call

--- incognia-wrapper.lisp
+++ incognia-wrapper.lisp
@@ -6,6 +6,8 @@
 
 (defvar *authentication-uri* (concatenate 'string *incognia-uri* "api/v1/token"))
 
+(defvar *onboarding-signups-uri* (concatenate 'string *incognia-uri* "api/v2/onboarding/signups"))
+
 (defun parse-yaml-file ()
   (yaml:parse (asdf:system-relative-pathname :incognia-wrapper #p"./credentials.yaml")))
 
@@ -21,3 +23,22 @@
   (get-access-token (dexador:post *authentication-uri*
                                   :basic-auth credentials
                                   :headers '(("Content-type" . "application/x-www-form-urlencoded")))))
+
+
+(defun onboarding-signups-request-body (installation-id address-line &optional app-id)
+  (jonathan:to-json (list :|installation_id| installation-id :|address_line| address-line :|app_id| app-id)))
+
+;; TODO: add GET method
+;; TODO: use keyword arguments
+(defun onboarding-signups (installation-id address-line)
+  (let* ((token (authenticate)))
+    (dexador:post *onboarding-signups-uri*
+                  :verbose t
+                  :headers (list
+                            '("Content-Type" . "application/json")
+                            (cons "Authorization" (concatenate 'string "Bearer " token)))
+                  :content (onboarding-signups-request-body installation-id address-line))))
+
+;; Example
+(incognia-apis:onboarding-signups "installation-id"
+                                   "address-line")
