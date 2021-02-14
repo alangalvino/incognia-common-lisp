e79ba6cfae3585278bd59cb9d0e2ae71598e39d4
Alan Gomes <alan@inloco.com.br>
2021-02-14 16:49:20 -0300

    Test plist remove null values

--- tests/util.lisp
+++ tests/util.lisp
@@ -8,12 +8,27 @@
 
 (deftest test-plist-remove-null-value
   (testing "should remove null values of a simple property list"
-    (let* ((plist '(:one-key 1 :other-key nil)))
+    (let* ((plist '(:key-1 1 :key-2 nil :key-3 3)))
       (ok (equal
            (incognia-wrapper.util:plist-remove-null-values plist)
-           '(:one-key 1)))))
-  (testing "should remove null values of a simple property list"
-    (let* ((plist '(:one-key 1 :other-key nil)))
+           '(:key-1 1 :key-3 3)))))
+  (testing "should remove null values of a nested (1 layer) property list"
+    (let* ((plist '(:key-1 1 :key-2 (:key-3 nil) :key-4 4)))
       (ok (equal
            (incognia-wrapper.util:plist-remove-null-values plist)
-           '(:one-key 1))))))
+           '(:key-1 1 :key-4 4)))))
+  (testing "should remove null values of a nested (2 layers) property list"
+    (let* ((plist '(:key-1 1 :key-2 (:key-3 (:key-4 nil)) :key-5 5)))
+      (ok (equal
+           (incognia-wrapper.util:plist-remove-null-values plist)
+           '(:key-1 1 :key-5 5)))))
+  (testing "should return an empty list"
+    (let* ((plist '(:key-1 nil)))
+      (ok (equal
+           (incognia-wrapper.util:plist-remove-null-values plist)
+           '()))))
+  (testing "should return an empty list"
+    (let* ((plist '()))
+      (ok (equal
+           (incognia-wrapper.util:plist-remove-null-values plist)
+           '())))))
