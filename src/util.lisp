0b784bbc1db4e5435ba73d7704006c4fbc441125
Alan Gomes <alan@inloco.com.br>
2021-02-14 13:56:59 -0300

    Implement in-depth plist remove null values

--- src/util.lisp
+++ src/util.lisp
@@ -10,19 +10,27 @@
   ;; ex. for tab equals 1 "~%1@t~a: ~a:"
   (format stream "~@[~%~v@t~a: ~] ~@[~a~]" tab key value))
 
-(defun hash-table-pretty-print (hash-table &optional (tab 0))
+(defun hash-table-pretty-print (hash-table &optional (tab 0) (stream t))
   (loop for key being the hash-keys of hash-table
           using (hash-value value)
         do (if (equal (type-of value) 'hash-table)
                (progn
-                 (hash-table-print-key-value-with-tab tab key)
+                 (hash-table-print-key-value-with-tab tab key nil stream)
                  (hash-table-pretty-print value (+ tab 1)))
-               (hash-table-print-key-value-with-tab tab key value))))
+               (hash-table-print-key-value-with-tab tab key value stream))))
 
-(defun plist-remove-null-values (plist)
-  (loop for (key value) on plist by #'cddr
+(defun plist-remove-null-values (plist &optional (remove-in-depth t))
+  (loop with new-plist = nil
+        with tmp-value = nil ;; will hold the result when value is a 'cons
+        for (key value) on plist by #'cddr
         when value
-          append (list key value)))
+          do (setf tmp-value value)
+             (if (and remove-in-depth
+                      (eq (type-of tmp-value) 'cons))
+                 (setf tmp-value (plist-remove-null-values value)))
+             (if tmp-value
+                 (setf new-plist (append new-plist (list key tmp-value))))
+        finally (return new-plist)))
 
 (defun pretty-print (response-string)
   (hash-table-pretty-print (jonathan:parse response-string :as :hash-table)))
