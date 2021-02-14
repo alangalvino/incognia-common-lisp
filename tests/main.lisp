a7af08bcd82478769854910b79b99c93efb5d920
Alan Gomes <alan@inloco.com.br>
2021-02-14 16:58:21 -0300

    Remove default test

--- tests/main.lisp
+++ tests/main.lisp
@@ -5,7 +5,3 @@
 (in-package :incognia-wrapper/tests/main)
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :incognia-wrapper)' in your Lisp.
-
-(deftest test-target-1
-  (testing "should (= 1 1) to be true"
-    (ok (= 1 1))))
