ae3dc7bab7cf586a76e1dbb0eb07c589f72c930d
Alan Gomes <alan@inloco.com.br>
2021-02-14 22:19:55 -0300

    Update run-tests.sh

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -7,6 +7,6 @@ ros run -- --version
 
 # Note: Assume that repository is checkout to workspace folder in previous step
 dir=/root/.roswell/local-projects/target
-sudo cp -R /github/workspace ${dir}
+sudo cp -R $GITHUB_WORKSPACE ${dir}
 cd ${dir}
 rove *.asd 2>&1 | tee /tmp/test.log
