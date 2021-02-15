fe0eaa894af55ac3bb5e942851d2717885f6ae04
Alan Gomes <alan@inloco.com.br>
2021-02-14 22:49:41 -0300

    Update run-tests.sh

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -5,7 +5,8 @@ set -eux
 ros use sbcl-bin
 ros run -- --version
 
-projectdir=~/.roswell/local-projects/target
+mkdir ~/common-lisp/
+projectdir=~/common-lisp/target
 cp -R $GITHUB_WORKSPACE ${projectdir}
 cd ${projectdir}
 ~/.roswell/bin/rove *.asd 2>&1 | tee /tmp/test.log
