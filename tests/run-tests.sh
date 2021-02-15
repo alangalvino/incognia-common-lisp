c5b66e5f6dfe8b402e2df7bb200dd893e600300f
Alan Gomes <alan@inloco.com.br>
2021-02-14 22:32:45 -0300

    Update run-tests.sh

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -5,5 +5,11 @@ set -eux
 ros use sbcl-bin
 ros run -- --version
 
-cd $GITHUB_WORKSPACE
+projectdir=~/.roswell/local-projects/target
+cp -R $GITHUB_WORKSPACE ${projectdir}
+cd ${projectdir}
 ~/.roswell/bin/rove *.asd 2>&1 | tee /tmp/test.log
+
+# Note: In Clozure CL, terminating debug console finishes in errcode 0,
+# so grep message to check if the test has actually run.
+grep -E "tests? passed" /tmp/test.log
