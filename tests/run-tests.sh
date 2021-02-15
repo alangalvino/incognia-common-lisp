90123c51b0ac18fc08b2a44e820e25e76ca8e5c6
Alan Gomes <alan@inloco.com.br>
2021-02-14 22:22:19 -0300

    Update run-tests.sh

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -5,8 +5,5 @@ set -eux
 ros use sbcl-bin
 ros run -- --version
 
-# Note: Assume that repository is checkout to workspace folder in previous step
-dir=/root/.roswell/local-projects/target
-sudo cp -R $GITHUB_WORKSPACE ${dir}
-cd ${dir}
+cd $GITHUB_WORKSPACE
 rove *.asd 2>&1 | tee /tmp/test.log
