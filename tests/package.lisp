710bf2537e601d97bbb2976475f129af8bd503d7
alan <1418294+alangalvino@users.noreply.github.com>
2021-05-23 23:31:33 -0300

    Removes deprecated config.yml tests and fix utils broken tests

---
+++ tests/package.lisp
@@ -0,0 +1,15 @@
+(defpackage incognia-wrapper
+  (:use :cl)
+  (:nicknames :incognia)
+  (:export
+           ;; For test only
+           :plist-remove-null-values
+
+           ;; Public functions
+           :configure
+           :make-address
+           :send-feedback
+           :register-signup
+           :get-signup-assessment
+           :register-payment
+           :register-login))
--- tests/utils.lisp
+++ tests/utils.lisp
@@ -1,8 +1,8 @@
-(defpackage incognia-wrapper/tests/util
+(defpackage incognia-wrapper/tests/utils
   (:use :cl
-        :incognia-wrapper.util
+        :incognia-wrapper
         :rove))
-(in-package :incognia-wrapper/tests/util)
+(in-package :incognia-wrapper/tests/utils)
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :incognia-wrapper)' in your Lisp.
 
@@ -10,25 +10,25 @@
   (testing "should remove null values of a simple property list"
     (let* ((plist '(:key-1 1 :key-2 nil :key-3 3)))
       (ok (equal
-           (incognia-wrapper.util:plist-remove-null-values plist)
+           (incognia:plist-remove-null-values plist)
            '(:key-1 1 :key-3 3)))))
   (testing "should remove null values of a nested (1 layer) property list"
     (let* ((plist '(:key-1 1 :key-2 (:key-3 nil) :key-4 4)))
       (ok (equal
-           (incognia-wrapper.util:plist-remove-null-values plist)
+           (incognia:plist-remove-null-values plist)
            '(:key-1 1 :key-4 4)))))
   (testing "should remove null values of a nested (2 layers) property list"
     (let* ((plist '(:key-1 1 :key-2 (:key-3 (:key-4 nil)) :key-5 5)))
       (ok (equal
-           (incognia-wrapper.util:plist-remove-null-values plist)
+           (incognia:plist-remove-null-values plist)
            '(:key-1 1 :key-5 5)))))
   (testing "should return an empty list"
     (let* ((plist '(:key-1 nil)))
       (ok (equal
-           (incognia-wrapper.util:plist-remove-null-values plist)
+           (incognia:plist-remove-null-values plist)
            '()))))
   (testing "should return an empty list"
     (let* ((plist '()))
       (ok (equal
-           (incognia-wrapper.util:plist-remove-null-values plist)
+           (incognia:plist-remove-null-values plist)
            '())))))
