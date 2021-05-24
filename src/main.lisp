ead1efb5cec7a021e8fb2882f6fa9202826fbf2c
alan <1418294+alangalvino@users.noreply.github.com>
2021-05-23 22:10:47 -0300

    Use address class on Incognia API method calls

--- src/main.lisp
+++ src/main.lisp
@@ -47,6 +47,7 @@
 
 (defun send-feedback (&key timestamp event installation-id account-id)
   (check-type event feedback-event-type)
+
   (do-auth-request
     :uri (incognia-uri *feedbacks-uri*)
     :method :post
@@ -60,23 +61,35 @@
     :uri (concatenate 'string  (incognia-uri *signups-uri*) signup-id)
     :method :get))
 
-(defun register-signup (&key installation-id address-line app-id)
-  (do-auth-request
-    :uri (incognia-uri *signups-uri*)
-    :method :post
-    :body (to-json (list :|installation_id| installation-id
-                         :|address_line| address-line
-                         :|app_id| app-id))))
+(defun register-signup (&key installation-id address app-id)
+  (assert installation-id)
+
+  (let* ((request-body (append
+                        (list :|installation_id| installation-id)
+                        (and app-id (list :|app_id| app-id))
+                        (and address (addr-line-valid-p address) (addr-line-plist address))
+                        (and address (addr-coordinates-valid-p address) (addr-coordinates-plist address))
+                        (and address (addr-structured-valid-p address) (addr-structured-plist address)))))
+
+    (do-auth-request
+      :uri (incognia-uri *signups-uri*)
+      :method :post
+      :body (to-json request-body))))
 
 (defun register-transaction (&key installation-id account-id type app-id addresses)
-  (do-auth-request
-    :uri (incognia-uri *transactions-uri*)
-    :method :post
-    :body (to-json `(:|installation_id| ,installation-id
-                     :|account_id| ,account-id
-                     :|type| ,type
-                     ,@(if addresses '(:|addresses| addresses))
-                     ,@(if app-id '(:|app_id| app-id))))))
+  (assert (and installation-id account-id type))
+
+  (let* ((request-body (append
+                        (list :|installation_id| installation-id)
+                        (list :|account_id| account-id)
+                        (list :|type| type)
+                        (and app-id (list :|app_id| app-id))
+                        (and addresses (mapcar #'addr-plist addresses)))))
+
+    (do-auth-request
+      :uri (incognia-uri *transactions-uri*)
+      :method :post
+      :body (to-json request-body))))
 
 (defun register-login (&key installation-id account-id app-id)
   (register-transaction :installation-id installation-id
