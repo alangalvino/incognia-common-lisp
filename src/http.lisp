fe52f62ef5b7bca77493e66d95002e54751afc2a
alan <1418294+alangalvino@users.noreply.github.com>
2021-05-26 19:41:16 -0300

    Implement http error condition

--- src/http.lisp
+++ src/http.lisp
@@ -1,11 +1,41 @@
 (in-package :cl-incognia)
 
+(define-condition http-request-error (error)
+  ((http-status :initarg :http-status
+                :initform nil
+                :accessor http-status)
+   (response-body :initarg :response-body
+                  :initform nil
+                  :accessor response-body)
+   (request-uri :initarg :request-uri
+                :initform nil
+                :accessor request-uri)
+   (request-method :initarg :request-method
+                   :initform nil
+                   :accessor request-method))
+  ;; the :report is the message into the debugger:
+  (:report (lambda (condition stream)
+             (let ((http-status (http-status condition))
+                   (request-method (request-method condition))
+                   (request-uri (request-uri condition))
+                   (response-body (response-body condition)))
+               (format stream "[http-request-error] status-code: ~D, request-method: ~D, request-uri: ~D, response-body: ~D" http-status request-method request-uri response-body)))))
+
 (defmacro do-request (&key uri method body basic-auth headers (parse-response t))
-  `(let* ((response (dex:request ,uri
-                                 :method ,method
-                                 :basic-auth ,basic-auth
-                                 :headers ,headers
-                                 :content ,body)))
+  `(let* ((response (handler-case (dex:request ,uri
+                                               :method ,method
+                                               :basic-auth ,basic-auth
+                                               :headers ,headers
+                                               :content ,body)
+                      (t (e)
+                        (let ((http-status (dex:response-status e))
+                              (response-body (dex:response-body e))
+                              (request-method (dex:request-method e))
+                              (request-uri (quri:render-uri (dex:request-uri e))))
+                          (error 'http-request-error :http-status http-status
+                                                     :response-body response-body
+                                                     :request-method request-method
+                                                     :request-uri request-uri))))))
      (if (and response ,parse-response)
          (parse-json response)
          response)))
