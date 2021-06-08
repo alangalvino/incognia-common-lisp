80ff2a0b4afe4bc11999cec48cb115f8591b252a
alan <1418294+alangalvino@users.noreply.github.com>
2021-06-08 14:44:01 -0300

    Refactor fetch auth-token function

--- src/auth.lisp
+++ src/auth.lisp
@@ -2,20 +2,24 @@
 
 (defvar *auth-token* nil)
 
-(defun revoke-token ()
+(defun revoke-auth-token ()
   (setf *auth-token* nil))
 
-(defun token-expires-in ()
-  (getf *auth-token* :|expires_in|))
+(defun token-expires-in (&optional (token *auth-token*))
+  (getf token :|expires_in|))
 
-(defun token-created-at ()
-  (getf *auth-token* :|created_at|))
+(defun token-created-at (&optional (token *auth-token*))
+  (getf token :|created_at|))
 
-(defun token-type ()
-  (getf *auth-token* :|token_type|))
+(defun token-type (&optional (token *auth-token*))
+  (getf token :|token_type|))
 
-(defun token-access-token ()
-  (getf *auth-token* :|access_token|))
+(defun token-access-token (&optional (token *auth-token*))
+  (getf token :|access_token|))
+
+(defun set-token-created-at (&key (token *auth-token*) created-at)
+  (setf (getf token :|created_at|) created-at)
+  token)
 
 (defun authenticate ()
   (assert (and (client-id) (client-secret) (region)))
@@ -28,12 +32,14 @@
 
 (defun auth-token-validp ()
   (let ((now (get-universal-time)))
-    (and *auth-token* (token-access-token) (> (parse-integer (token-expires-in)) (- now (token-created-at))))))
+    (and (token-access-token) (> (parse-integer (token-expires-in)) (- now (token-created-at))))))
 
-(defun update-token ()
-  (setf *auth-token* (authenticate))
-  (setf (getf *auth-token* :|created_at|) (get-universal-time))
-  *auth-token*)
+(defun request-auth-token ()
+  (let* ((auth-token (authenticate))
+         (now (get-universal-time)))
+    (set-token-created-at :token auth-token :created-at now)))
 
-(defun auth-token ()
-  (if (auth-token-validp) *auth-token* (update-token)))
+(defun fetch-auth-token ()
+  (if (auth-token-validp)
+      *auth-token*
+      (setf *auth-token* (request-auth-token))))
